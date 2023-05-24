84341d64be50a42da2b1a636997057668bd20427
amr099 <amr70210@gmail.com>
2023-05-24 19:55:37 +0300

    f

--- src/Components/Nav/Search.jsx
+++ src/Components/Nav/Search.jsx
@@ -1,11 +1,17 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 export default function Search() {
     const [param, setParam] = useState("");
+
     return (
-        <div className='flex shrink bg-sky-100 rounded-2xl items-center justify-between w-72 md:w-[30%]'>
+        <form
+            onSubmit={redirect(
+                `https://react-store123.netlify.app/products/search?q=${param}`
+            )}
+            className='flex shrink bg-sky-100 rounded-2xl items-center justify-between w-72 md:w-[30%]'
+        >
             <input
                 onChange={(e) => setParam(e.target.value)}
                 type='search'
@@ -13,11 +19,10 @@ export default function Search() {
                 id='search'
                 className='w-[90%] shrink bg-inherit pl-4 py-2 rounded-l-2xl focus:outline-none focus:bg-inherit'
             />
-            <Link
-                to={`https://react-store123.netlify.app/products/search?q=${param}`}
-            >
+
+            <button>
                 <MagnifyingGlassIcon className='w-6 md:w-10 h-7 md:h-7 px-1' />
-            </Link>
-        </div>
+            </button>
+        </form>
     );
 }
